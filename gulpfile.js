860a3b28daa634e5be5c06a2f200d0fadf394a85
Caleb Nance <calebnance@gmail.com>
2016-12-10 21:05:00 -0500

    gulp started

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+// https://markgoodyear.com/2014/01/getting-started-with-gulp/
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
 gulp.task('default', function() {
-  
+
 });
